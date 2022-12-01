596998fc3ee501a062742c011ffe948cc09d3499
thexler <g.axler@gmail.com>
2022-11-30 16:15:07 -0800

    local install

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+
+setup(
+    name="DL Argpeggion",
+    version="0.0.1",
+    author="Gregory Axler",
+    requires=["einops", "equinox" "jax", "jaxtyping", "numpy", "optax", "toml"],
+)
