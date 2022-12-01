24b70b30786d0d932a3878e6189bab3bf3ca4b79
Gregory Axler <g.axler@gmail.com>
2022-11-30 16:22:28 -0800

    Delete scratch.py

--- scratch.py
+++
@@ -1,5 +0,0 @@
-from fabric import Connection
-import os
-IP_ = "34.27.255.59"
-conn = Connection(IP_, connect_kwargs={"key_filename": os.path.expanduser('~/.ssh/google_compute_engine'), })  
-print("df")
